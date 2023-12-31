1dc10ac0020f7f0b4cfa9faf7cab02e1a5a3162f
anshbhatia04 <anshbhatia04@gmail.com>
2023-12-31 22:58:29 +0530

    Update!

---
+++ index.js
@@ -0,0 +1,42 @@
+const express= require("express");
+const { users } = require("./data/users.json");
+const { books } = require("./data/books.json");
+
+const app = express();
+
+const PORT = 8081;
+
+app.use(express.json());
+
+app.get("/", (req, res)=>{
+    res.status(200).json({
+        message: "Server is up and running :-)",
+        data: "hey",
+    });
+});
+
+/**
+ * Route: /users
+ * Method: GET
+ * Description: Get all users
+ * Access: Public
+ * Parameters: None
+ */
+
+app.get("/users", (req, res) => {
+    res.status(200).json({
+      success: true,
+      data: users,
+    });
+  });
+
+app.get("*", (req, res)=>{
+    res.status(404).json({
+        message: "This route doesn't exist",
+    });
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Server is listening on port ${PORT}`);
+});
+
